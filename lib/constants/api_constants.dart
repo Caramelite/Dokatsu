c014f61463a37a9ce168633f5285f912cc7f5340
Jules Russel Lucero <jules.lucero.20@usjr.edu.ph>
2021-07-27 22:52:46 +0800

    Created controllers and services

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,2 +1,2 @@
-
-const api_key = 'a3e9d1b2-c2b8-4fbe-934d-d7f2632e1fdc';
+const cats_api_key = 'a3e9d1b2-c2b8-4fbe-934d-d7f2632e1fdc';
+const dogs_api_key = 'da7921ee-f0fe-4de0-9adf-13aa224119b3';
---
+++ lib/services/dogs_services.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+
+class DogsServices {
+  static var client = http.Client();
+
+}
