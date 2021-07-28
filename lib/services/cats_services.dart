0a09e84d6244fb871f071494b00115d912fb7a8e
Carmela Trujillo <carmelatrujillo17@gmail.com>
2021-07-28 12:59:31 +0800

    cat category

--- lib/services/cats_services.dart
+++ lib/services/cats_services.dart
@@ -1,8 +1,19 @@
-import 'dart:io';
-import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:katsuu/model/cat.dart';
 
 class CatsServices {
   static var client = http.Client();
-
+  static Future<List<Category>> fetchCategory() async {
+    var response =
+        await client.get(Uri.parse('https://api.thedogapi.com/v1/categories'));
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      List<Category> category = categoryFromJson(jsonString);
+      return category;
+      // return <the list of product from the jsonString>;
+    } else {
+      //Get.snackbar("", "Please reload the app.");
+      return [];
+    }
+  }
 }
--- lib/services/dogs_services.dart
+++ lib/services/dogs_services.dart
@@ -1,8 +1,5 @@
-import 'dart:io';
-import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class DogsServices {
   static var client = http.Client();
-
 }
