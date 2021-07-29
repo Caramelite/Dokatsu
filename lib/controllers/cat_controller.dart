683ae2566fe08fa319b5d1a34dacfdc617c16bb8
Jules Russel Lucero <jules.lucero.20@usjr.edu.ph>
2021-07-30 00:59:24 +0800

    Removed category in cats

--- lib/controllers/cat_controller.dart
+++ lib/controllers/cat_controller.dart
@@ -1,25 +1,18 @@
 import 'package:dokatsu/models/Cats/CatBreed.dart';
-import 'package:dokatsu/models/Cats/CatCategory.dart';
 import 'package:dokatsu/services/cat_services.dart';
 import 'package:get/get.dart';
 
 class CatController extends GetxController {
   static CatController instance = Get.find();
   var isLoading = true.obs;
-  var categoryList = <CatCategory>[].obs;
   var breedList = <CatBreed>[].obs;
   @override
   void onInit() {
-    fetchCategory();
     fetchBreed();
     fetchImages();
     super.onInit();
   }
 
-  void fetchCategory() async {
-    categoryList.value = await CatServices.fetchCategories();
-    isLoading.value = false;
-  }
 
   void fetchBreed() async {
     breedList.value = await CatServices.fetchBreeds();
--- lib/services/cat_services.dart
+++ lib/services/cat_services.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 
 import 'package:dokatsu/constants/api_constants.dart';
 import 'package:dokatsu/models/Cats/CatBreed.dart';
-import 'package:dokatsu/models/Cats/CatCategory.dart';
 import 'package:http/http.dart' as http;
 
 class CatServices {
@@ -27,33 +26,9 @@ class CatServices {
         headers: {HttpHeaders.authorizationHeader: cats_api_key});
     if (response.statusCode == 200) {
       var jsonString = response.body;
-      List<String> imageUrls = List<String>.from(json.decode(jsonString).map((x) => x['url']).toList());
+      List<String> imageUrls = List<String>.from(
+          json.decode(jsonString).map((x) => x['url']).toList());
       return imageUrls;
-    } else
-      return [];
-  }
-
-  static Future<List<CatCategory>> fetchCategories() async {
-    var response = await client.get(
-        Uri.parse('https://api.thecatapi.com/v1/categories'),
-        headers: {HttpHeaders.authorizationHeader: cats_api_key});
-    if (response.statusCode == 200) {
-      var jsonString = response.body;
-      return categoryFromJson(jsonString);
-    } else
-      return [];
-  }
-
-  static Future<List<String>> fetchImages({int id = -1}) async {
-    final uri = id != -1
-        ? 'https://api.thecatapi.com/v1/images/search?limit=50category_ids=$id'
-        : 'https://api.thecatapi.com/v1/images/search?limit=50';
-    var response = await client.get(Uri.parse(uri),
-        headers: {HttpHeaders.authorizationHeader: cats_api_key});
-    if (response.statusCode == 200) {
-      var jsonString = response.body;
-      print(json.decode(jsonString));
-      return [];
     } else
       return [];
   }
